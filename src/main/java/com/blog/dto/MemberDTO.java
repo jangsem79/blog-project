4fb2550b6a242c157657b3d5c308eef750f7f770
jangsu <jangsam04@nate.com>
2026-02-27 08:33:24 +0900

    회원가입 기능 구현

--- src/main/java/com/blog/dto/MemberDTO.java
+++ src/main/java/com/blog/dto/MemberDTO.java
@@ -2,4 +2,10 @@ package com.blog.dto;
 
 public class MemberDTO {
 
+	private int id;
+    private String username;
+    private String password;
+    private String email;
+    private String createdAt;
+    // getter, setter
 }
--- src/main/java/com/blog/mapper/Member.java
+++ src/main/java/com/blog/mapper/Member.java
@@ -2,4 +2,7 @@ package com.blog.mapper;
 
 public interface Member {
 
+	int insertUser(UserDto user);
+    UserDto selectUserByUsername(String username);
+    UserDto selectUserByEmail(String email);
 }
